c4e33f22dcffdb0a19a08ae7639bc148d7ffd737
Bry Cox <carl.cc@gmail.com>
2019-05-23 08:31:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
       end
     elsif req.path.match(/add/)
       item = req.params["item"]
+      binding.pry
       if @@items.include?(item)
         @@cart << item
         resp.write "added #{item}"
