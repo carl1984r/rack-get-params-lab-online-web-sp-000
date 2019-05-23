f619863a5bc1556f592cd521915b0a969bf0dcd8
Bry Cox <carl.cc@gmail.com>
2019-05-23 07:56:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,6 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        binding.pry
         resp.write "Your cart is empty"
 
 
