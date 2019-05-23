78031524da0f3e2bcfe654e19cda1959ebfe92dd
Bry Cox <carl.cc@gmail.com>
2019-05-23 08:05:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,9 +17,9 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-      #else
-      #  @@cart.each do |item|
-      #  resp.write "#{item}\n"
+      else
+        @@cart.each do |item|
+        resp.write "#{item}\n"
       end
     else
       resp.write "Path Not Found"
